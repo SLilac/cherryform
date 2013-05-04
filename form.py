78d2a8cd085fe2bd389809e80b6737189812ec20
Lilac <scatteredlilac@gmail.com>
2013-05-05 00:23:01 +0800

    Update form.py

--- form.py
+++ form.py
@@ -4,10 +4,13 @@ from fileds import Filed
 
 class MetaForm(type):
 
-  def __new__(cls, classname, basecalss, classdict):
-      self.__fileds__ = bassclass.__fileds
-      [ self.__fileds__.append(filed) if type(filed) is Filed for filed in classname.__dict__]
+    def __new__(cls, classname, basecalss, classdict):
+    
+        self.__fileds__ = bassclass.__fileds
+        [ self.__fileds__.append(filed) if type(filed) is Filed for filed in classname.__dict__]
         
 
 
-class From(meataclass = MetaForm)
+class Form(meataclass = MetaForm)
+    self.__name__ = Form.__class__.name
+    def __init__(sefl)
