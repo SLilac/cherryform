2271f8d8921022eb6633ecb8d1a259d8f254821e
Lilac <scatteredlilac@gmail.com>
2013-05-05 00:25:11 +0800

    Update form.py

--- form.py
+++ form.py
@@ -6,7 +6,7 @@ class MetaForm(type):
 
     def __new__(cls, classname, basecalss, classdict):
     
-        self.__fileds__ = bassclass.__fileds
+        self.__fileds__ = bassclass.__fileds__
         [ self.__fileds__.append(filed) if type(filed) is Filed for filed in classname.__dict__]
         
 
